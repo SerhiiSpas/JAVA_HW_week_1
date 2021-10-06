9565b59d8fe36cfcae6fd44e1fc66161f4db7e31
Serhii Spasennikov <sergeys@wizardsdev.com>
2021-10-06 16:11:34 +0300

    Initial commit

---
+++ src/week_1/four_number.java
@@ -0,0 +1,11 @@
+package week_1;
+
+public class four_number {
+    /*
+     * Даны 4 числа типа int. Сравнить их и вывести наименьшее на консоль.
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
