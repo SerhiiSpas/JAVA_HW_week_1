6dabbeeafaf222a0553b34e41ee9df1d74be6855
Serhii <43697311+SerhiiSpas@users.noreply.github.com>
2021-10-07 09:48:41 +0300

    Done ver 1.1

---
+++ src/week_1/max_number.java
@@ -0,0 +1,41 @@
+package week_1;
+
+import java.util.Scanner;
+
+public class max_number {
+    public static void main(String[] args) {
+
+        int max = 0;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a first number: ");
+        int num1 = in.nextInt();
+        System.out.print("Input a second number: ");
+        int num2 = in.nextInt();
+        System.out.print("Input a third number: ");
+        int num3 = in.nextInt();
+        System.out.print("Input a fourth number: ");
+        int num4 = in.nextInt();
+
+        if (num1 >= num2 && num1 >= num3 && num1 >= num4)
+        {
+            max++;
+        }
+
+        if (num2 >= num1 && num2 >= num3 && num2 >= num4)
+        {
+            max++;
+        }
+
+        if (num3 >= num1 && num3 >= num2 && num3 >= num4)
+        {
+            max++;
+        }
+
+        if (num4 >= num1 && num4 >= num2 && num4 >= num3)
+        {
+            max++;
+        }
+        System.out.println("Maximum number of numbers " + max);
+    }
+}
