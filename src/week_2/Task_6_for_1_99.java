f500d8778ca01cee25b2e03ca8d6e34e34b485af
Serhii <43697311+SerhiiSpas@users.noreply.github.com>
2021-10-11 14:17:32 +0300

    Task 6 done

---
+++ src/week_2/Task_6_for_1_99.java
@@ -0,0 +1,13 @@
+package week_2;
+
+public class Task_6_for_1_99 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 99; i++)
+        {
+            if (i % 2 != 0)
+            {
+                System.out.println(i + ", ");
+            }
+        }
+    }
+}
